d95ed8b80dd25e8b90e9940d0d7e292e92fc8f5b
THANHDAT1011 <thanhdat24110905@gmail.com>
2025-02-24 14:35:45 +0700

    dat

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,11 @@ namespace bai13_TinhCANchi
 {
     public partial class Form1 : Form
     {
+        string[] canArr = { "Canh", "tân", "Nhân", "Quí", "Ất", "Bính", "Đinh", "Mậu", "Kỷ" ,"Giáp"};
+        string[] chiArr = { "Thìn", "Tỵ", "Ngọ", "Mùi", "Dậu", "Tất", "Hợi", "Sửu", "Dần", "Mão","Tý","Mạo"};
+        string TinhCan(int nam){ return canArr[nam % 10];}
+        string TinhChi(int nam) { return chiArr[nam % 12]; }
+        string TinhCanChi(string text) {return TinhCan(Convert.ToInt32(text)) + "" + TinhChi(Convert.ToInt32(text));}
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +24,11 @@ namespace bai13_TinhCANchi
 
         private void btnTinh_Click(object sender, EventArgs e)
         {
-            string[] canArr = { "Canh", "tân", "Nhân", "Quí", "Ất", "Bính", "Đinh", "Mậu", "Kỷ" };
-            string[] chiArr = { "Thìn", "Tỵ", "Ngọ", "Mùi", "Dậu", "Tất", "Hợi", "Sửu", "Dần", "Mão" }; 
+            txtkq.Text = TinhCanChi(txtnam.Text);
+            for (int i = 1900; i < 3000; i++) 
+            {
+                lstNames.Items.Add(i + " - " + TinhCanChi(i.ToString()));
+            }
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
