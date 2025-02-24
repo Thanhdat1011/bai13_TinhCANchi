eff1f242474358b53bb16619452de6ad59d677e7
ADMIN <ADMIN@17-LAB01>
2025-02-24 11:20:59 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace bai13_TinhCANchi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Năm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kết Quả";
+            // 
+            // txtnam
+            // 
+            this.txtnam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnam.Location = new System.Drawing.Point(211, 100);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(171, 26);
+            this.txtnam.TabIndex = 1;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkq.Location = new System.Drawing.Point(211, 169);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(171, 26);
+            this.txtkq.TabIndex = 1;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(426, 153);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(119, 42);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Exit";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinh.Location = new System.Drawing.Point(426, 84);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(119, 42);
+            this.btnTinh.TabIndex = 2;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.Location = new System.Drawing.Point(551, 50);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(233, 238);
+            this.lstNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 459);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtnam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.ListBox lstNames;
+    }
+}
+
